b193c7286e1a8982b614a4bfe225d6116827d452
Sebastian Fritzsche <sebifr58@gmx.de>
2023-10-10 20:44:24 +0200

    Grundgerüst

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TextAdventure
+{
+    class Programm
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Herzlich Willkommen zu diesem Spiel.");
+            Console.WriteLine("Drücke eine beliebige Taste zum Fortfahren");
+            Console.ReadKey();
+            Console.WriteLine("Wähle aus zwischen:");
+            Console.WriteLine("1) Spiel starten");
+            Console.WriteLine("2) Spiel beenden");
+            int start = int.Parse(Console.ReadLine());
+            Console.WriteLine(start);
+            if (start == 1)
+            {
+                spielGestartet();
+            }
+            if (start == 1)
+            Console.ReadKey();
+        }
+
+        public static void spielGestartet()
+        {
+            Console.WriteLine("Spielstart");
+        }
+
+        public static void spielBeendet()
+        {
+           
+        }
+    }
+}
